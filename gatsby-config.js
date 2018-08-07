43ae64978e4b527440b77ff0a088dcceb7b2b67b
Brahim Sourny <brahimsourny@Brahims-Mac-Pro.local>
2018-08-07 19:46:28 +0200

    add source-file

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,16 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    'gatsby-transformer-remark'
+
   ],
 }
